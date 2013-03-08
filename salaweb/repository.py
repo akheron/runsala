371e3370204f6c0ec08894383a31f22813205033
Petri Lehtinen <petri@digip.org>
2013-03-08 07:17:14 +0200

    wip

--- salaweb/repository.py
+++ salaweb/repository.py
@@ -1,6 +1,33 @@
+import os
+from django.conf import settings
+
+
 class Repository(object):
     def __init__(self, name):
         self.name = name
+        self.path = os.path.join(
+            settings.SALAWEB_DATADIR,
+            'repositories',
+            name,
+        )
+
+    def exists(self):
+        return os.path.isdir(self.path)
 
     def as_html(self):
-        pass
+        def walk(start, depth=0):
+            for node in sorted(os.listdir(start)):
+                fullpath = os.path.join(start, node)
+                if os.path.isdir(fullpath):
+                    yield '<div class="dir depth%d">' % depth
+
+                    # me wants yield from :(
+                    for fragment in walk(fullpath, depth + 1):
+                        yield fragment
+
+                    yield '</div>'
+
+                else:
+                    yield '<div class="file depth%d">' % depth
+                    yield cgi.escape(node)
+                    yield '</div>'
